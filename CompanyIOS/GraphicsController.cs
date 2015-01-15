1b346ea27b75f7f65aa98505cdda74804e9ae8e3
Nickolas- <kolyashpotenko@gmail.com>
2015-01-16 00:43:57 +0200

    Added new funcional for graphics control

--- CompanyIOS/GraphicsController.cs
+++ CompanyIOS/GraphicsController.cs
@@ -3,6 +3,7 @@ using Foundation;
 using UIKit;
 using System.CodeDom.Compiler;
 using CoreGraphics;
+using System.Collections.Generic;
 
 namespace CompanyIOS
 {
@@ -12,6 +13,7 @@ namespace CompanyIOS
 		public GraphicsViewWorker _cost;
 		public GraphicsViewWorker _quality;
 		public GraphicsViewWorker _service;
+		public GraphicsViewWorker _all;
 		public Tuple<string,GraphicsCallback> _data;
 		private bool _buttonAllCliked;
 		private bool _buttonCostCliked;
@@ -46,21 +48,50 @@ namespace CompanyIOS
 			btnQuarter.TouchUpInside += btnTopMenuTouch;
 			btnHalfYear.TouchUpInside += btnTopMenuTouch;
 			btnYear.TouchUpInside += btnTopMenuTouch;
-			btnCost.TouchUpInside += btnServicesTouch;
+			btnCost.TouchUpInside += btnOneTouch;
 
-			btnQuality.TouchUpInside += btnServicesTouch;
-			btnService.TouchUpInside += btnServicesTouch;
-			btnAll.TouchUpInside += btnServicesTouch;
+			btnQuality.TouchUpInside += btnTwoTouch;
+			btnService.TouchUpInside += btnThreeTouch;
+			btnAll.TouchUpInside += btnFourTouch;
 
 			HttpServiceConn conn = new HttpServiceConn ();
-			_Questions = await conn.GetQuestions (Token, Company);
-			btnCost.SetTitle (_Questions.Item2.Resource [0].QName, UIControlState.Normal);
+			try {
+				_Questions = await conn.GetQuestions (Token, Company);			
+				CreateButtonForGraphics (btnCost,btnQuality,btnService,btnAll);
+			} catch (Exception ex) {
+				UIAlertView error = new UIAlertView ("Ошибка", ex.Message, null, "Закрыть", null);
+				error.Show ();
+			}
+		
 			btnMonth.SendActionForControlEvents (UIControlEvent.TouchUpInside);
+
 			UISwipeGestureRecognizer gest = new UISwipeGestureRecognizer();
 			gest.Direction = UISwipeGestureRecognizerDirection.Right;
 			gest.AddTarget (() => MoveBack (gest));
 			MainBackGroubd.AddGestureRecognizer (gest);
+			ServiceWindow.BackgroundColor = new UIColor (246, 246, 246, 1);
+		}
 
+		void CreateButtonForGraphics (params UIButton[] buttons)
+		{
+			for (int i = 0; i < buttons.Length; i++) {
+				try {
+					string text = _Questions.Item2.Resource [i].QName;
+					buttons [i].TitleEdgeInsets = new UIEdgeInsets(20,0,0,0);
+					buttons [i].SetTitle (text, UIControlState.Normal);
+					buttons [i].TitleLabel.TextAlignment = UITextAlignment.Left;
+					buttons [i].TitleLabel.LineBreakMode = UILineBreakMode.WordWrap;
+					buttons [i].SetTitleColor (UIColor.FromRGB (150, 87, 162), UIControlState.Normal);
+					buttons [i].BackgroundColor = UIColor.FromRGB (246, 246, 246);
+					buttons [i].Font = UIFont.FromName (@"Helvetica-Light", 12);
+				} catch (ArgumentOutOfRangeException) {
+					buttons [i].SetTitle (string.Empty, UIControlState.Normal);
+					buttons [i].SetTitleColor (UIColor.FromRGB (150, 87, 162), UIControlState.Normal);
+					buttons [i].BackgroundColor = UIColor.FromRGB (246, 246, 246);
+					buttons [i].Font = UIFont.FromName (@"Helvetica-Light", 12);
+					buttons [i].Enabled = false;
+				}
+			}
 		}
 
 		void MoveBack (UISwipeGestureRecognizer gest)
@@ -80,42 +111,23 @@ namespace CompanyIOS
 			}
 			UIButton but = sender as UIButton;
 			HttpServiceConn conn = new HttpServiceConn ();
-			if (_buttonCostCliked && _buttonQualityCliked && _buttonServiceCliked) {
-				_buttonAllCliked = true;
-				_buttonCostCliked = false;
-				_buttonQualityCliked = false;
-				_buttonServiceCliked = false;
-			} else {
-				_buttonAllCliked = false;
-			}
 			if (_buttonAllCliked) {
-				_cost.DeleteTouchView ();
-				_cost.RemoveFromSuperview ();
-				btnCost.SetImage (new UIImage ("стоимость_а.png"), UIControlState.Normal);
-				_quality.DeleteTouchView ();
-				_quality.RemoveFromSuperview ();
-				btnQuality.SetImage (new UIImage ("качество_а.png"), UIControlState.Normal);
-				_service.DeleteTouchView ();
-				_service.RemoveFromSuperview ();
-				btnService.SetImage (new UIImage ("сервис_а.png"), UIControlState.Normal);
+				_all.DeleteTouchView ();
+				_all.RemoveFromSuperview ();
 			}
 			if (_buttonCostCliked) {
 				_cost.DeleteTouchView ();
 				_cost.RemoveFromSuperview ();
-				btnCost.SetImage (new UIImage ("стоимость_а.png"), UIControlState.Normal);
 			}
 			if (_buttonQualityCliked) {
 				_quality.DeleteTouchView ();
 				_quality.RemoveFromSuperview ();
-				btnQuality.SetImage (new UIImage ("качество_а.png"), UIControlState.Normal);
 			}
 			if (_buttonServiceCliked) {
 				_service.DeleteTouchView ();
-				_service.RemoveFromSuperview ();
-				btnService.SetImage (new UIImage ("сервис_а.png"), UIControlState.Normal);			
+				_service.RemoveFromSuperview ();						
 			}
-			loadingOverlay = new LoadingOverlay (new CGRect (MainBackGroubd.Frame.X, MainBackGroubd.Layer.Frame.Y, MainBackGroubd.Frame.Width, MainBackGroubd.Layer.Frame.Height - BottomMenu.Layer.Frame.Height));
-			 
+			loadingOverlay = new LoadingOverlay (new CGRect (MainBackGroubd.Frame.X, MainBackGroubd.Layer.Frame.Y, MainBackGroubd.Frame.Width, MainBackGroubd.Layer.Frame.Height - BottomMenu.Layer.Frame.Height));			 
 			View.Add (loadingOverlay);
 			_data = await conn.GetGraphicAsync (Token, DateTime.Now.ToString ("dd.MM.yyyy"), but.Title (UIControlState.Normal));
 			loadingOverlay.Hide ();
@@ -124,7 +136,7 @@ namespace CompanyIOS
 					CreateOsXMonthOrDaysVector (_data.Item2.Resource);
 					but.Enabled = false;
 					if (UIControlState.Disabled == but.State) {
-						but.TitleEdgeInsets = new UIEdgeInsets (0, - 46, 0, 0);
+						but.TitleEdgeInsets = new UIEdgeInsets (0, -46, 0, 0);
 					}
 					if (_but == null) {
 						_but = but;
@@ -135,27 +147,22 @@ namespace CompanyIOS
 						}
 						_but = but;
 					}
+					if (_buttonCostCliked) {
+						_buttonCostCliked = false;
+						btnCost.SendActionForControlEvents (UIControlEvent.TouchUpInside);
+					}
+					if (_buttonQualityCliked) {
+						_buttonQualityCliked = false;
+						btnQuality.SendActionForControlEvents (UIControlEvent.TouchUpInside);
+					}
+					if (_buttonServiceCliked) {
+						_buttonServiceCliked = false;
+						btnService.SendActionForControlEvents (UIControlEvent.TouchUpInside);
+					}
 					if (_buttonAllCliked) {
 						_buttonAllCliked = false;
 						btnAll.SendActionForControlEvents (UIControlEvent.TouchUpInside);
-					} else {
-						if (_buttonCostCliked) {
-							_buttonCostCliked = false;
-							btnCost.SendActionForControlEvents (UIControlEvent.TouchUpInside);
-						} else {
-							if (!_buttonAllCliked && !_buttonQualityCliked && !_buttonServiceCliked) {
-								btnCost.SendActionForControlEvents (UIControlEvent.TouchUpInside);
-							}
-						}
-						if (_buttonQualityCliked) {
-							_buttonQualityCliked = false;
-							btnQuality.SendActionForControlEvents (UIControlEvent.TouchUpInside);
-						}
-						if (_buttonServiceCliked) {
-							_buttonServiceCliked = false;
-							btnService.SendActionForControlEvents (UIControlEvent.TouchUpInside);
-						}
-					}
+					}					
 				} else {
 					UIAlertView error = new UIAlertView ("Ошибка", _data.Item1, null, "Закрыть", null);
 					error.Show ();
@@ -164,40 +171,33 @@ namespace CompanyIOS
 
 		}
 
-		void btnServicesTouch (object sender, EventArgs e)
+		void btnOneTouch (object sender, EventArgs e)
 		{
-			UIButton button = sender as UIButton;
-	
-			switch (button.Title (UIControlState.Normal)) {
-			case "cost":
-				CostGraphicDraw ();
-				break;
-			case "quality":
-				QualityGraphicDraw ();
-				break;
-			case "service":
-				ServiceGraphicDraw ();
-				break;
-			case "all":
-				AllGraphicDraw ();
-				break;
-			}
-
+			CostGraphicDraw ();	
+		}
+		void btnTwoTouch (object sender, EventArgs e)
+		{
+			QualityGraphicDraw ();	
+		}
+		void btnThreeTouch (object sender, EventArgs e)
+		{
+			ServiceGraphicDraw ();		
+		}
+		void btnFourTouch (object sender, EventArgs e)
+		{
+			AllGraphicDraw ();
 		}
 
 		void CostGraphicDraw ()
 		{
 
 			if (_buttonCostCliked) {
-				btnCost.SetImage (new UIImage ("стоимость_а.png"), UIControlState.Normal);
 				_cost.DeleteTouchView ();
 				_cost.RemoveFromSuperview ();
 				_buttonCostCliked = false;
-				_buttonAllCliked = false;
 			} else {
 				_cost = new GraphicsViewWorker (_data.Item2, WhatServiceWork.Cost, UIColor.FromRGB (71, 201, 175), GraphicViewWindow.Bounds,GraphicViewWindow);
 				GraphicViewWindow.AddSubview (_cost);
-				btnCost.SetImage (new UIImage ("стоимость_б.png"), UIControlState.Normal);
 				_buttonCostCliked = true;
 			}
 		}
@@ -205,79 +205,39 @@ namespace CompanyIOS
 		void QualityGraphicDraw ()
 		{
 			if (_buttonQualityCliked) {
-				btnQuality.SetImage (new UIImage ("качество_а.png"), UIControlState.Normal);
 				_quality.DeleteTouchView ();
 				_quality.RemoveFromSuperview ();
-				_buttonQualityCliked = false;
-				_buttonAllCliked = false;
+				_buttonQualityCliked = false;			
 			} else {
 				_quality = new GraphicsViewWorker (_data.Item2, WhatServiceWork.Quality, UIColor.FromRGB (255, 110, 23), GraphicViewWindow.Bounds,GraphicViewWindow);
 				GraphicViewWindow.AddSubview (_quality);
 				_buttonQualityCliked = true;
-				btnQuality.SetImage (new UIImage ("качество_б.png"), UIControlState.Normal);
 			}
 		}
 
 		void ServiceGraphicDraw ()
 		{
 			if (_buttonServiceCliked) {
-				btnService.SetImage (new UIImage ("сервис_а.png"), UIControlState.Normal);
 				_service.DeleteTouchView ();
 				_service.RemoveFromSuperview ();
-				_buttonServiceCliked = false;
-				_buttonAllCliked = false;
+				_buttonServiceCliked = false;			
 			} else {
 				_service = new GraphicsViewWorker (_data.Item2, WhatServiceWork.Service, UIColor.FromRGB (61, 147, 219), GraphicViewWindow.Bounds,GraphicViewWindow);
 				GraphicViewWindow.AddSubview (_service);
 				_buttonServiceCliked = true;
-				btnService.SetImage (new UIImage ("сервис_б.png"), UIControlState.Normal);
 			}
 		}
 
 		void AllGraphicDraw ()
 		{
-			if (_buttonCostCliked && _buttonQualityCliked && _buttonServiceCliked) {
-				_buttonAllCliked = true;
-				_buttonCostCliked = false;
-				_buttonQualityCliked = false;
-				_buttonServiceCliked = false;
-			} else {
+			if (_buttonServiceCliked) {
+				_all.DeleteTouchView ();
+				_all.RemoveFromSuperview ();
 				_buttonAllCliked = false;
-			}
-			if (_buttonAllCliked) {
-				_cost.DeleteTouchView ();
-				_quality.DeleteTouchView ();
-				_service.DeleteTouchView ();
-				_cost.RemoveFromSuperview ();
-				_quality.RemoveFromSuperview ();
-				_service.RemoveFromSuperview ();
-				_buttonAllCliked = false;
-				_buttonCostCliked = false;
-				_buttonQualityCliked = false;
-				_buttonServiceCliked = false;
-				btnCost.SetImage (new UIImage ("стоимость_а.png"), UIControlState.Normal);
-				btnQuality.SetImage (new UIImage ("качество_а.png"), UIControlState.Normal);
-				btnService.SetImage (new UIImage ("сервис_а.png"), UIControlState.Normal);
 			} else {
-				if (!_buttonCostCliked) {
-					_cost = new GraphicsViewWorker (_data.Item2, WhatServiceWork.Cost, UIColor.FromRGB (71, 201, 175), GraphicViewWindow.Bounds,GraphicViewWindow);
-					GraphicViewWindow.AddSubview (_cost);
-				}
-				if (!_buttonQualityCliked) {
-					_quality = new GraphicsViewWorker (_data.Item2, WhatServiceWork.Quality, UIColor.FromRGB (255, 110, 23), GraphicViewWindow.Bounds,GraphicViewWindow);
-					GraphicViewWindow.AddSubview (_quality);
-				}
-				if (!_buttonServiceCliked) {
-					_service = new GraphicsViewWorker (_data.Item2, WhatServiceWork.Service, UIColor.FromRGB (61, 147, 219), GraphicViewWindow.Bounds,GraphicViewWindow);
-					GraphicViewWindow.AddSubview (_service);
-				}
-				_buttonAllCliked = true;
-				_buttonCostCliked = true;
-				_buttonQualityCliked = true;
-				_buttonServiceCliked = true;
-				btnCost.SetImage (new UIImage ("стоимость_б.png"), UIControlState.Normal);
-				btnQuality.SetImage (new UIImage ("качество_б.png"), UIControlState.Normal);
-				btnService.SetImage (new UIImage ("сервис_б.png"), UIControlState.Normal);
+				_service = new GraphicsViewWorker (_data.Item2, WhatServiceWork.Service, UIColor.FromRGB (61, 147, 219), GraphicViewWindow.Bounds,GraphicViewWindow);
+				GraphicViewWindow.AddSubview (_service);
+				_buttonAllCliked = true;			
 			}
 		}
 
